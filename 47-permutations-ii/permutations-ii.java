c1dc0110e82ae4dd0e944ac5b2a4191e91fcc016
Pranav Kokate <74059877+Pranav141@users.noreply.github.com>
2025-11-07 16:56:41 +0530

    Time: 35 ms (16.07%) | Memory: 47 MB (5.39%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,19 +1,23 @@
 class Solution {
-    public void solve(List<List<Integer>> res,List<Integer> arr,int[] nums,boolean[] visited){
+    public void solve(List<List<Integer>> res,List<Integer> arr,int[] nums
+    ,boolean[] visited,HashSet<List<Integer>> st){
         if(arr.size() == nums.length){
-            for(int i=0;i<res.size();i++){
-                if(res.get(i).equals(arr)){
-                    return;
-                }
+            // for(int i=0;i<res.size();i++){
+            //     if(res.get(i).equals(arr)){
+            //         return;
+            //     }
+            // }
+            st.add(new ArrayList<>(arr));
+            if(st.size() == res.size()+1){
+                res.add(new ArrayList<>(arr));
             }
-            res.add(new ArrayList<>(arr));
             return;
         }
         for(int i=0;i<nums.length;i++){
             if(visited[i]==false){
                 visited[i]=true;
                 arr.add(nums[i]);
-                solve(res,arr,nums,visited);
+                solve(res,arr,nums,visited,st);
                 arr.remove(arr.size()-1);
                 visited[i]=false;
             }
@@ -24,7 +28,8 @@ class Solution {
         boolean[] visited = new boolean[nums.length];
         Arrays.fill(visited,false);
         List<List<Integer>> res = new ArrayList<>();
-        solve(res,new ArrayList<>(),nums,visited);
+        HashSet<List<Integer>> st = new HashSet<>();
+        solve(res,new ArrayList<>(),nums,visited,st);
         return res;
     }
 }
