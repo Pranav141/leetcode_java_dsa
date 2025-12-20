76956197e2d8acc50ee32a7d35e77aa2dcfb0a6b
Pranav Kokate <74059877+Pranav141@users.noreply.github.com>
2025-12-20 17:57:03 +0530

    Time: 2 ms (93.92%) | Memory: 46 MB (10.24%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,23 +1,26 @@
 class Solution {
-    public void solve(List<List<Integer>> res,int[] can,int k,int idx,List<Integer> comb){
-        if(k==0){
+    public void solve(List<List<Integer>> res,List<Integer> comb,int[] arr,int target,int idx){
+        if(idx==arr.length){
+            return;
+        }
+        if(target == 0){
             res.add(new ArrayList<>(comb));
             return;
         }
-        if(idx<0){
-            return;
-        }
-        solve(res,can,k,idx-1,comb);
-        if(can[idx]<=k){
-            comb.add(can[idx]);
-            solve(res,can,k-can[idx],idx,comb);
-            comb.remove(comb.size()-1);
-        }
-        return;
+
+        
+            if(target>=arr[idx]){
+                comb.add(arr[idx]);
+                solve(res,comb,arr,target-arr[idx],idx);
+                comb.removeLast();
+            }
+                solve(res,comb,arr,target,idx+1);
+            
+        
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res=new ArrayList<>();
-        solve(res,candidates,target,candidates.length-1,new ArrayList<>());
+        List<List<Integer>> res = new ArrayList<>();
+        solve(res,new ArrayList<>(),candidates,target,0);
         return res;
     }
 }
