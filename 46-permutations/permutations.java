74c9533e8e5f09a7cb10bea484d56d4276fb670a
Pranav Kokate <74059877+Pranav141@users.noreply.github.com>
2026-05-17 18:37:31 +0530

    Time: 1 ms (86.86%) | Memory: 45.5 MB (45.92%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,25 @@
 class Solution {
-    public void solve(List<List<Integer>> res,List<Integer> arr,int[] nums,boolean[] visited,int idx){
-        if(arr.size()==nums.length){
-            res.add(new ArrayList<>(arr));
+    public void solve(List<List<Integer>> res,List<Integer> temp,boolean[] visited,int[] nums){
+        if(temp.size() == nums.length){
+            res.add(new ArrayList<>(temp));
             return;
         }
+
         for(int i=0;i<nums.length;i++){
-            if(visited[i]==false){
+            if(visited[i] == false){
+                temp.add(nums[i]);
                 visited[i]=true;
-                arr.add(nums[i]);
-                solve(res,arr,nums,visited,idx+1);
-                arr.remove(arr.size()-1);
+                solve(res,temp,visited,nums);
+                temp.removeLast();
                 visited[i]=false;
             }
         }
     }
+
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        boolean[] visited=new boolean[nums.length];
-        Arrays.fill(visited,false);
-        solve(res,new ArrayList<>(),nums,visited,0);
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        solve(res,new ArrayList<>(),visited,nums);
         return res;
     }
 }
